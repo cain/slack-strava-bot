6001b6d07bf39821c2afac529022a619084813bf
Cain Hall <cain.hall98@gmail.com>
2021-07-30 15:23:13 +1000

    Try another approach

--- util/screenshot.tsx
+++ util/screenshot.tsx
@@ -1,26 +1,49 @@
 
-const puppeteer = require('puppeteer');
+import chromium from 'chrome-aws-lambda'
+
+async function getBrowserInstance() {
+	const executablePath = await chromium.executablePath
+
+	if (!executablePath) {
+		// running locally
+		const puppeteer = require('puppeteer')
+		return puppeteer.launch({
+			args: chromium.args,
+			headless: true,
+			ignoreHTTPSErrors: true
+		})
+	}
+
+	return chromium.puppeteer.launch({
+		args: chromium.args,
+		executablePath,
+		headless: chromium.headless,
+		ignoreHTTPSErrors: true
+	})
+}
 
 export function generateMap({ polyline, id }: { polyline: string, id: number }) {
   return new Promise<{ path: string }>(async (res, rej) => {
     try {
-      let chrome: any = {};
-      const config = {
-        headless: true,
-      }
-      let puppeteer;
-      if (process.env.APP_ENV !== 'local') {
-        // running on the Vercel platform.
-        chrome = require('chrome-aws-lambda');
-        puppeteer = require('puppeteer-core');
-      } else {
-        // running locally.
-        puppeteer = require('puppeteer');
-      }
-      const browser = await puppeteer.launch({
-        args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
-        executablePath: await chrome.executablePath,
-      });
+      // let chrome: any = {};
+      // const config = {
+      //   headless: true,
+      // }
+      // let puppeteer;
+      // if (process.env.APP_ENV !== 'local') {
+      //   // running on the Vercel platform.
+      //   chrome = require('chrome-aws-lambda');
+      //   puppeteer = require('puppeteer-core');
+      // } else {
+      //   // running locally.
+      //   puppeteer = require('puppeteer');
+      // }
+      // const browser = await puppeteer.launch({
+      //   args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+      //   executablePath: await chrome.executablePath,
+      // });
+      let browser = null
+      browser = await getBrowserInstance()
 
       const page = await browser.newPage();
     
