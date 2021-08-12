558dd95ed763cf960a28fc96fba843ea796a69ec
Cain Hall <cain.hall98@gmail.com>
2021-08-12 17:44:13 +1000

    Slack command return 200 response

--- pages/api/slack/command.tsx
+++ pages/api/slack/command.tsx
@@ -1,7 +1,7 @@
-const { getActivity } = require('../../util/strava');
 
 export default async function handler (req, res) {
   if(req.method === 'POST') {
     res.status(200).json('bip bop bap');
+    return;
   }
 }
